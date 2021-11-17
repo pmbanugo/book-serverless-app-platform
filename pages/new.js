824fbaa7295eb94dcb9dd7c1b657113a3d983e01
Peter Mbanugo <peter@foobar.agency>
2021-11-17 09:29:07 +0100

    add get started button and get app url from config

--- pages/new.js
+++ pages/new.js
@@ -1,30 +1,49 @@
-import Link from 'next/link'
 import { useState } from 'react'
+import Link from 'next/link'
 import { DropDownList } from '@progress/kendo-react-dropdowns'
 import { filterBy } from '@progress/kendo-data-query'
 
-export default function NewApp({ repos }) {
-  const filterChange = ({ filter }) => {
-    setData(filterBy(repos.slice(), filter))
+export const getStaticProps = async () => {
+  return {
+    props: { githubAppUrl: process.env.APP_URL },
   }
+}
+
+export default function NewApp({ repos, githubAppUrl, installationId }) {
+  const filterChange = ({ filter }) => {}
 
   return (
     <>
-      <h2>Select the GitHub repository you want to deploy</h2>
-      <div className="info">
-        Can't see your repository here?{' '}
-        <Link href="https://github.com/apps/tekton-kn-app/installations/new">
-          <a>Configure the GitHub app permission</a>
-        </Link>
-      </div>
-      <div>
-        <DropDownList
-          style={{ width: '26rem', margin: '1rem' }}
-          data={repos}
-          filterable={true}
-          onFilterChange={filterChange}
-        />
-      </div>
+      {!repos ? (
+        <div>
+          Get started by{' '}
+          <Link href={`${githubAppUrl}/installations/new`}>
+            <span className="k-button">
+              connecting to your GitHub account/organsation
+            </span>
+          </Link>
+        </div>
+      ) : (
+        <>
+          <h2>Select the GitHub repository you want to deploy</h2>
+          <div className="info">
+            Can't see your repository here?{' '}
+            <Link
+              href={`https://github.com/settings/installations/${installationId}`}
+            >
+              <a>Configure the GitHub app permission</a>
+            </Link>
+          </div>
+          <div>
+            <DropDownList
+              style={{ width: '26rem', margin: '1rem' }}
+              data={repos}
+              filterable={true}
+              onFilterChange={filterChange}
+            />
+          </div>
+        </>
+      )}
 
       <style jsx>
         {`
